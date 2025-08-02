c3ddda09ca5166a51463a83b72a4e945737326f8
ckw28502 <ckw28502@gmail.com>
2025-08-02 20:58:35 +0200

    feat: add configuration for PostgreSQL connection and update migration scripts

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(env.POSTGRES_DB, env.POSTGRES_USER, env.POSTGRES
     host: env.POSTGRES_HOST,     // Database server hostname or IP address
     port: env.POSTGRES_PORT,     // Port on which the database listens (default 5432)
     dialect: 'postgres',         // Specify the SQL dialect for PostgreSQL
-    logging: env.NODE_ENV === 'dev' ? console.log : false,  // Enable logging of SQL queries only in development environment for easier debugging
+    logging: env.NODE_ENV === 'prod' ? false : console.log,  // Enable logging of SQL queries only in development environment for easier debugging
 });
 
 export default sequelize;
